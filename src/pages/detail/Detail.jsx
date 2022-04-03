fc797ef1349dd7843961a52f8e9b98ad9f47c260
unknown <izacaqsha@gmail.com>
2022-04-03 17:35:39 +0700

    is it working? i dont know, hey im trying

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -43,80 +43,82 @@ const Detail = () => {
 
   let result;
   if (isReady) {
-    const checkAvailability = storageDetail.findIndex((object) => {
-      return object.id === detail.id;
-    });
-    result = (
-      <div className="detailLayout">
-        <div
-          className="backdrop"
-          style={{
-            backgroundImage: `url(${
-              detail.backdrop_path
-                ? `https://image.tmdb.org/t/p/w500${detail.backdrop_path}`
-                : `https://via.placeholder.com/400x400?text=Image+Not+available`
-            })`,
-          }}
-        >
-          <section>
-            <div className="containerDetail">
-              <div
-                className="posterDetail"
-                style={{
-                  backgroundImage: `url(${
-                    detail.poster_path
-                      ? `https://image.tmdb.org/t/p/w500${detail.poster_path}`
-                      : `https://via.placeholder.com/400x400?text=Image+Not+available`
-                  })`,
-                }}
-              ></div>
-              <div className="isiDetail">
-                <h1 className="detailTitle">{detail.title}</h1>
-                <div className="ratingBintang">
-                  <h2>
-                    <i className="fa-solid fa-star"></i>{" "}
-                    {detail.vote_average ? detail.vote_average / 2 : "-"} / 5
-                  </h2>
+    if (storageDetail) {
+      const checkAvailability = storageDetail.findIndex((object) => {
+        return object.id === detail.id;
+      });
+      result = (
+        <div className="detailLayout">
+          <div
+            className="backdrop"
+            style={{
+              backgroundImage: `url(${
+                detail.backdrop_path
+                  ? `https://image.tmdb.org/t/p/w500${detail.backdrop_path}`
+                  : `https://via.placeholder.com/400x400?text=Image+Not+available`
+              })`,
+            }}
+          >
+            <section>
+              <div className="containerDetail">
+                <div
+                  className="posterDetail"
+                  style={{
+                    backgroundImage: `url(${
+                      detail.poster_path
+                        ? `https://image.tmdb.org/t/p/w500${detail.poster_path}`
+                        : `https://via.placeholder.com/400x400?text=Image+Not+available`
+                    })`,
+                  }}
+                ></div>
+                <div className="isiDetail">
+                  <h1 className="detailTitle">{detail.title}</h1>
+                  <div className="ratingBintang">
+                    <h2>
+                      <i className="fa-solid fa-star"></i>{" "}
+                      {detail.vote_average ? detail.vote_average / 2 : "-"} / 5
+                    </h2>
+                  </div>
+                  <div className="containerGenre">
+                    {genres.map((item) => {
+                      return (
+                        <div key={item.id} className="genre">
+                          {item.name}
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <p>release date: {detail.release_date}</p>
+                  <p>{detail.overview}</p>
                 </div>
-                <div className="containerGenre">
-                  {genres.map((item) => {
-                    return (
-                      <div key={item.id} className="genre">
-                        {item.name}
-                      </div>
-                    );
-                  })}
-                </div>
-                <p>release date: {detail.release_date}</p>
-                <p>{detail.overview}</p>
               </div>
-            </div>
-            <div
-              className="btn btn-primary buttonAddFavorite"
-              onClick={() => {
-                if (storageDetail == null) {
-                  localStorage.setItem("detail", JSON.stringify([detail]));
-                } else if (checkAvailability === -1) {
-                  localStorage.setItem(
-                    "detail",
-                    JSON.stringify(storageDetail.concat(detail))
-                  );
-                }
-                setButtonTrigger(!buttonTrigger);
-              }}
-            >
-              {checkAvailability === -1 ? (
-                <span>Add to Favorite Collection</span>
-              ) : (
-                <span className="AddedWarning">Already in Collection</span>
-              )}
-            </div>
-          </section>
+              <div
+                className="btn btn-primary buttonAddFavorite"
+                onClick={() => {
+                  if (storageDetail == null) {
+                    localStorage.setItem("detail", JSON.stringify([detail]));
+                  } else if (checkAvailability === -1) {
+                    localStorage.setItem(
+                      "detail",
+                      JSON.stringify(storageDetail.concat(detail))
+                    );
+                  }
+                  setButtonTrigger(!buttonTrigger);
+                }}
+              >
+                {checkAvailability === -1 ? (
+                  <span>Add to Favorite Collection</span>
+                ) : (
+                  <span className="AddedWarning">Already in Collection</span>
+                )}
+              </div>
+            </section>
+          </div>
+          <Introduction text="Movies TRAILERS!!" />
+          <VideoCard videos={videos} />
         </div>
-        <Introduction text="Movies TRAILERS!!" />
-        <VideoCard videos={videos} />
-      </div>
-    );
+      );
+    }
   } else {
     result = <LoadSpin />;
   }
